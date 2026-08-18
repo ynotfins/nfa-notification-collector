0a23b7a3058d7cd73525bff0e26aa39d5fa466ea
R3lentless_Grind <ynotfins@gmail.com>
2026-08-18 07:07:50 -0400

    test: migrate Compose picker test API

--- app/src/androidTest/java/com/nfaalerts/collector/ui/sources/SourcePickerScreenInstrumentedTest.kt
+++ app/src/androidTest/java/com/nfaalerts/collector/ui/sources/SourcePickerScreenInstrumentedTest.kt
@@ -5,7 +5,7 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import androidx.activity.compose.setContent
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.junit4.v2.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
